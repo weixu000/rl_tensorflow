81b81adb578828aded8d8273c8ba4e292c502339
weixu000 <wei-x15@mails.tsinghua.edu.cn>
2017-04-19 19:25:48 +0800

    again

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ from DQN.target import *
 ENV_NAME = 'CartPole-v0'
 N_TEST = 100
 GOAL = 195
-MAX_EPISODES = 400
+MAX_EPISODES = 200
 
 
 # ENV_NAME = 'CartPole-v1'
 # N_TEST = 100
 # GOAL = 475
-# MAX_EPISODES = 500
+# MAX_EPISODES = 200
 
 
 # ENV_NAME = 'MountainCar-v0'
@@ -44,22 +44,22 @@ def play_episode(env, action_select, perceive, render=False):
         if done: return ret
 
 
-def train_episodes(env, agent, n_episodes=500):
+def train_episodes(env, agent, n_test, goal, n_episodes=500):
     rewards, aver_rewards = [], []
     try:
         for i_episode in range(n_episodes):
             # if i_episode % N_TEST == 0: play(env, agent.greedy_action, None, True)
             rewards.append(play_episode(env, agent.epsilon_greedy, agent.perceive))
 
-            aver_rewards.append(np.sum(rewards[-min(N_TEST, len(rewards)):]) / min(N_TEST, len(rewards)))
+            aver_rewards.append(np.sum(rewards[-min(n_test, len(rewards)):]) / min(n_test, len(rewards)))
             print("Episode {} finished with test average reward {:.2f}".format(i_episode, aver_rewards[-1]))
-            if len(rewards) > N_TEST and aver_rewards[-1] >= GOAL:
+            if len(rewards) > n_test and aver_rewards[-1] >= goal:
                 print("Environment solved after {} episodes".format(i_episode))  # 按openai gym 要求完成了环境
                 break
         else:
             r = 0
-            for _ in range(N_TEST): r += play_episode(env, agent.greedy_action, None)
-            r /= N_TEST
+            for _ in range(n_test): r += play_episode(env, agent.greedy_action, None)
+            r /= n_test
             print("Maximum {} of episodes exceeded with greedy average reward {}".format(n_episodes, r))
     finally:
         return rewards, aver_rewards
@@ -70,9 +70,9 @@ def save_rewards(log_dir, rewards, aver_rewards):
         pickle.dump((rewards, aver_rewards), f)
 
 
-def plot_rewards(rewards, aver_rewards):
+def plot_rewards(rewards, aver_rewards, n_aver):
     plt.plot(rewards, label='Return for each episode')
-    plt.plot(aver_rewards, label='Average return for last {} episodes'.format(N_TEST))
+    plt.plot(aver_rewards, label='Average return for last {} episodes'.format(n_aver))
     plt.legend(frameon=False)
     plt.xlabel('Episode')
     plt.ylabel('Return')
@@ -86,13 +86,13 @@ def main():
     agent = DQN(env.observation_space.shape, env.action_space.n, log_dir,
                 FCFeatures([20, 20]),
                 OriginalQLayer([10]),
-                RandomReplay(5000, 100),
-                OriginalDQN(),
-                1E-3, 0.3, 0, 0.95, 2000)
+                RandomReplay(10000, 200),
+                DoubleDQN(),
+                2E-3, 0.4, 0, 0.95, 2000)
     agent.save_hyperparameters()
-    rewards, aver_rewards = train_episodes(env, agent, MAX_EPISODES)
+    rewards, aver_rewards = train_episodes(env, agent, N_TEST, GOAL, MAX_EPISODES)
     save_rewards(agent.log_dir, rewards, aver_rewards)
-    plot_rewards(rewards, aver_rewards)
+    plot_rewards(rewards, aver_rewards, N_TEST)
 
 
 if __name__ == "__main__":
