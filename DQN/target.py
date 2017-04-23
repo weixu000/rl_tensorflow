5d4ad1d3ed24cf77ecc1620f8a6b307fa167f879
weixu000 <wei-x15@mails.tsinghua.edu.cn>
2017-04-23 10:45:23 +0800

    SingleDQN now has a target network as the paper proposed

--- DQN/target.py
+++ DQN/target.py
@@ -7,9 +7,6 @@ class Target:
     产生目标Q值基类
     """
 
-    def __init__(self, GAMMA=1):
-        self.GAMMA = GAMMA
-
     def create_target(self, init, input_layer, Q_layer):
         """
         初始化Target
@@ -19,14 +16,8 @@ class Target:
         """
         self._input_layer = input_layer
         self._Q_layer = Q_layer
-        self._create_sessions(init)
-
-    def _create_sessions(self, init):
-        """
-        建立session
-        :param init: tf初始化所有variable
-        """
-        raise NotImplementedError()
+        self._sessions = [tf.Session(), tf.Session()]
+        for s in self._sessions: s.run(init)
 
     def compute_y(self, batch):
         """
@@ -35,33 +26,44 @@ class Target:
         raise NotImplementedError()
 
     def __getitem__(self, state):
-        raise NotImplementedError
+        ret = np.zeros(self._Q_layer.shape[1])
+        for s in self._sessions:
+            ret += s.run(self._Q_layer, feed_dict={self._input_layer: [state]})[0]
+        return ret / len(self._sessions)
 
     def save_hyperparameters(self):
         return dict(filter(lambda x: x[0][0].isupper(), self.__dict__.items()))
 
 
-class OriginalDQN(Target):
+class SingleDQN(Target):
     """
     Nature DQN
     """
 
-    def __getitem__(self, state):
-        return self._session.run(self._Q_layer, feed_dict={self._input_layer: [state]})[0]
-
-    def _create_sessions(self, init):
-        self._session = tf.Session()
-        self._session.run(init)
+    def __init__(self, GAMMA=1, TARGET_HOLD=200):
+        self.GAMMA = GAMMA
+        self.TARGET_HOLD = TARGET_HOLD
+        self._n_compute = 0
 
     def compute_y(self, batch):
-        state_batch, action_batch, y_batch, nxt_state_batch, done_batch = batch
+        self._n_compute += 1
+        if self._n_compute % self.TARGET_HOLD == 0: self.__copy_vars()
 
+        state_batch, action_batch, y_batch, nxt_state_batch, done_batch = batch
         # 计算公式中的maxQ，如果完成设为0
-        nxt_qs = np.max(self._session.run(self._Q_layer, feed_dict={self._input_layer: nxt_state_batch}), axis=1)
+        nxt_qs = np.max(self._sessions[1].run(self._Q_layer, feed_dict={self._input_layer: nxt_state_batch}), axis=1)
         nxt_qs[done_batch] = 0
         y_batch += self.GAMMA * nxt_qs  # 计算公式，y在抽取时已经保存了reward
 
-        return self._session, batch
+        return self._sessions[0], batch
+
+    def __copy_vars(self):
+        """
+        更新目标网络
+        """
+        with self._sessions[0].graph.as_default():
+            for v in tf.trainable_variables():
+                self._sessions[1].run(v.assign(self._sessions[0].run(v)))
 
 
 class DoubleDQN(Target):
@@ -69,15 +71,8 @@ class DoubleDQN(Target):
     Double DQN
     """
 
-    def __getitem__(self, state):
-        ret = np.zeros(self._Q_layer.shape[1])
-        for s in self._sessions:
-            ret += s.run(self._Q_layer, feed_dict={self._input_layer: [state]})[0]
-        return ret / len(self._sessions)
-
-    def _create_sessions(self, init):
-        self._sessions = [tf.Session(), tf.Session()]
-        for s in self._sessions: s.run(init)
+    def __init__(self, GAMMA=1):
+        self.GAMMA = GAMMA
 
     def compute_y(self, batch):
         state_batch, action_batch, y_batch, nxt_state_batch, done_batch = batch
